180fd0965216cd3f9a1a007ad1105ad86a8675f2
fourofclubs <fourofclubs@gmail.com>
2021-04-11 23:28:08 -0500

    Added more stuff

--- src/Gen.hs
+++ src/Gen.hs
@@ -1,15 +1,25 @@
-module Gen
-  ( Field,
-    Class,
-  )
-where
+module Gen where
 
 import Control.Monad (join)
+import Data.Char (toLower)
 import qualified Data.List as L
 
 data Field = Field {fieldName :: String, fieldType :: Either String Class, description :: String}
 
 data Class = Class {className :: String, classFields :: [Field]}
+
+classLines :: Bool -> Class -> [String]
+classLines static clss =
+  ["public " ++ if static then "static" else "" ++ "final class " ++ className clss ++ " extends Data<" ++ className clss ++ "> {"]
+    ++ fmap indent (classBody clss)
+    ++ ["}"]
+
+classBody :: Class -> [String]
+classBody clss =
+  staticDeclarations clss
+    ++ fieldDeclarations (classFields clss)
+    ++ "" : constructor clss
+    ++ "" : factoryMethod clss
 
 indent :: String -> String
 indent = ("\t" ++)
@@ -26,18 +36,66 @@ fieldDeclaration fld =
 fieldDeclarations :: [Field] -> [String]
 fieldDeclarations = (>>= fieldDeclaration)
 
+fieldParams :: [Field] -> String
+fieldParams = join . L.intersperse ", " . fmap fieldParam
+  where
+    fieldParam fld = "final " ++ fieldTypeName fld ++ " " ++ fieldName fld
+
+fieldList :: [Field] -> String
+fieldList = L.intercalate ", " . fmap fieldName
+
 constructor :: Class -> [String]
 constructor clss =
-  ["private final " ++ className clss ++ "(" ++ params ++ ") {"]
+  [ "private final " ++ className clss ++ "(" ++ fieldParams (classFields clss) ++ ") {",
+    indent "super(" ++ equalName clss ++ ", " ++ hashName clss ++ ", " ++ showName clss ++ ");"
+  ]
     ++ fmap indent fieldSetters
     ++ ["}"]
   where
-    fieldParam fld = "final " ++ fieldTypeName fld ++ " " ++ fieldName fld
     fields = classFields clss
     fieldSetter fld = "this." ++ fieldName fld ++ "=" ++ fieldName fld ++ ";"
-    params = join $ L.intersperse ", " (fmap fieldParam fields)
     fieldSetters = fmap fieldSetter fields
 
-fieldParams :: [Field] -> String
-fieldParams fields = join (L.intersperse ", " (fmap fieldParam fields))
-    where fieldParam fld = "final " ++ fieldTypeName fld ++ " " ++ fieldName fld
+staticDeclarations :: Class -> [String]
+staticDeclarations clss = [equalDeclaration, hashDeclaration, showDeclaration] >>= ($ clss)
+
+equalDeclaration :: Class -> [String]
+equalDeclaration clss =
+  [ "/** The {@link " ++ className clss ++ "} {@link Equal} instance */",
+    "public static final Equal<" ++ className clss ++ "> " ++ equalName clss ++ ";"
+  ]
+
+hashDeclaration :: Class -> [String]
+hashDeclaration clss =
+  [ "/** The {@link " ++ className clss ++ "} {@link Hash} instance */",
+    "public static final Hash<" ++ className clss ++ "> " ++ hashName clss ++ ";"
+  ]
+
+showDeclaration :: Class -> [String]
+showDeclaration clss =
+  [ "/** The {@link " ++ className clss ++ "} {@link Show} instance */",
+    "public static final Show<" ++ className clss ++ "> " ++ showName clss ++ ";"
+  ]
+
+equalName :: Class -> String
+equalName = (++ "Equal") . toLowerHead . className
+
+hashName :: Class -> String
+hashName = (++ "Hash") . toLowerHead . className
+
+showName :: Class -> String
+showName = (++ "Show") . toLowerHead . className
+
+toLowerHead :: String -> String
+toLowerHead [] = []
+toLowerHead (s : ss) = toLower s : ss
+
+factoryMethod :: Class -> [String]
+factoryMethod clss =
+  [ "public static final " ++ name ++ " " ++ toLowerHead name ++ "(" ++ fieldParams (classFields clss) ++ "){",
+    indent ("return " ++ constructorCall ++ ";"),
+    "}"
+  ]
+  where
+    name = className clss
+    constructorCall = "new " ++ name ++ "(" ++ fieldList (classFields clss) ++ ")"
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,10 @@
+import Gen
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = putStrLn . unlines . classLines False $ class1
+
+field1 = Field "field1" (Left "Option<String>") "The first test field"
+
+field2 = Field "field2" (Left "Integer") "The second test field"
+
+class1 = Class "TestClass" [field1, field2]
